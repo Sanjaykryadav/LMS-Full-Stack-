df1c5dc1c3b70789b28ec2ce657d8e9e3901ed6e
SANJAY KUMAR YADAV <127543232+Sanjaykryadav@users.noreply.github.com>
2025-04-17 17:05:36 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,6 @@ import "bootstrap/dist/js/bootstrap.js";
 import axios from "axios";
 axios.defaults.withCredentials = true;
 
-export const backend_server = `https://lms-full-stack-5.onrender.com`;
+export const backend_server = `https://lms-full-stack-7.onrender.com`;
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App></App>);
